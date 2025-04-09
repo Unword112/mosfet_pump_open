6a855cdd8586de5b86f43a97d5ed4722a431b7e0
Unword112 <pog123@hotmail.co.uk>
2025-04-09 19:19:39 +0700

    deep sleep

--- sketch_apr8a.ino
+++ sketch_apr8a.ino
@@ -10,11 +10,13 @@ const char* password = "12345678n";
 // กำหนดค่าเวลา
 #define SOIL_SENSOR_PIN 33
 #define PUMP_PIN 26
+#define pH_SENSOR 33
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", 0, 60000); // ใช้เวลา UTC, รีเฟรชทุกๆ 60 วินาที
 
 int soilValue = 0;
+int pHValue= 0;
 const int dryThreshold = 3500;
 
 void setup() {
@@ -34,11 +36,6 @@ void setup() {
   timeClient.begin();
   timeClient.setTimeOffset(7 * 3600); // ปรับเวลาเป็น GMT+7 (สำหรับแสดงผลใน Serial)
 
-  // ตั้งเวลาให้ ESP32 ตื่นทุกๆ 1 ชั่วโมง (3600 วินาที)
-  //esp_sleep_enable_timer_wakeup(3600 * 1000000); 
-}
-
-void loop() {
   // อัพเดตเวลา NTP
   timeClient.update();
   String currentTime = timeClient.getFormattedTime(); // ดึงเวลาปัจจุบันในรูปแบบ hh:mm:ss
@@ -49,26 +46,49 @@ void loop() {
   delay(5000);
 
   soilValue = analogRead(SOIL_SENSOR_PIN);
+  pHValue = analogRead(pH_SENSOR);
+
+  Serial.println(pHValue);
 
   if (soilValue < dryThreshold) {
     digitalWrite(PUMP_PIN, HIGH); 
-    Serial.println("Pump ON");
+      digitalWrite(PUMP_PIN, HIGH);  // เปิดปั๊ม
+      Serial.println("Pump ON at the scheduled time");
+      delay(5000);  // เปิดปั๊ม 1 ชั่วโมง
+      digitalWrite(PUMP_PIN, LOW);   // ปิดปั๊ม
+      Serial.println("Pump OFF");
   } else {
     digitalWrite(PUMP_PIN, LOW); 
   }
 
-  // เช็คว่าเวลาตรงกับ 8:00 AM หรือ 5:00 PM
-  if (currentTime == "08:00:00" || currentTime == "17:00:00") {
-    digitalWrite(PUMP_PIN, HIGH);  // เปิดปั๊ม
-    Serial.println("Pump ON at the scheduled time");
-    delay(3600000);  // เปิดปั๊ม 1 ชั่วโมง
-    digitalWrite(PUMP_PIN, LOW);   // ปิดปั๊ม
-    Serial.println("Pump OFF");
-  }
+  int firstColon = currentTime.indexOf(':');
+  int secondColon = currentTime.indexOf(':', firstColon + 1);
 
+  String minuteStr = currentTime.substring(firstColon + 1, secondColon);
+  int minute = minuteStr.toInt();
+
+  // เช็คว่าเวลาตรงกับ 8:00 AM หรือ 5:00 PM
+  if (currentTime.startsWith("08") || currentTime.startsWith("17"))  {
+    if(minute >= 0 && minute <= 30){ // เช็คเวลาหลักนาที 8:00 - 8:30 || 17:00 - 17:30
+      digitalWrite(PUMP_PIN, HIGH);  // เปิดปั๊ม
+      Serial.println("Pump ON at the scheduled time");
+      delay(5000);  // เปิดปั๊ม 1 ชั่วโมง
+      digitalWrite(PUMP_PIN, LOW);   // ปิดปั๊ม
+      Serial.println("Pump OFF");
+    }
+  }
+  // ตั้งเวลาให้ ESP32 ตื่นทุกๆ 1 ชั่วโมง (3600 วินาที)
+  Serial.println("Wake up...");
+  //esp_sleep_enable_timer_wakeup(3600 * 1000000);
+  esp_sleep_enable_timer_wakeup(36 * 1000000);
+  
   Serial.flush(); 
   Serial.println("Entering Deep Sleep...");
 
   // เข้าสู่โหมด deep sleep
-  //esp_deep_sleep_start();
+  esp_deep_sleep_start();
+}
+
+void loop() {
+  
 }

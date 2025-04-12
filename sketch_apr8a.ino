60b37b1340b6afbe840613ddb9ac3029906c423b
Bsl Nathakith <54128035+Unword112@users.noreply.github.com>
2025-04-12 03:39:55 -0800

    Update sketch_apr8a.ino

--- sketch_apr8a.ino
+++ sketch_apr8a.ino
@@ -1,11 +1,21 @@
 #include <WiFi.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+#include <PubSubClient.h>
+//#include <DHT.h>
 #include "esp_sleep.h"
 
 // กำหนดค่า Wi-Fi
 const char* ssid = "iPhonePok";
 const char* password = "12345678n";
+
+const char* mqtt_server = "demo.thingsboard.io";
+const char* ID = "d71fa310-1751-11f0-b194-bf88b865708c";
+const char* token = "5XayITvut6awxdSuZgtq";
+const int port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 // กำหนดค่าเวลา
 #define SOIL_SENSOR_PIN 33
@@ -18,8 +28,37 @@ WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", 0, 60000); // ใช้เวลา UTC, รีเฟรชทุกๆ 60 วินาที
 
 int soilValue = 0;
-int pHValue= 0;
+float pHValue= 0;
+float temp = 0;
 const int dryThreshold = 3500;
+
+void reconnect() {
+  while (!client.connected()) {
+    if (client.connect(ID, token, "")) {
+      client.subscribe("v1/devices/me/rpc/request/+"); // Subscribe RPC
+    } else {
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String message;
+  for (unsigned int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+  Serial.println("Received: " + message);
+
+}
+
+void sendData(float temp, float pH, int soil) {
+  String payload = "{\"temperature\": " + String(temp, 1) +
+                   ", \"pH_Value\": " + String(pH, 1) +
+                   ", \"Soil_Value\": " + String(soil) + "}";
+  client.publish("v1/devices/me/telemetry", payload.c_str());
+  Serial.println("Sent to ThingsBoard: " + payload);
+  delay(5000);
+}
 
 void setup() {
   Serial.begin(115200);
@@ -49,11 +88,20 @@ void setup() {
   delay(5000);
 
   soilValue = analogRead(SOIL_SENSOR_PIN);
-  pHValue = analogRead(pH_SENSOR);
+  //pHValue = analogRead(pH_SENSOR);
 
   delay(1000);
   esp_sleep_enable_ext0_wakeup(WAKE_PIN, 0); // ปลุกเมื่อ GPIO4 = 0 (LOW)
   Serial.println(WAKE_PIN);
+
+  client.setServer(mqtt_server, port);
+  client.setCallback(callback);
+  client.loop();
+
+  //dht.begin();
+  //temp = dht.readTemperature();
+
+  reconnect();
 
   esp_sleep_wakeup_cause_t wakeup_reason = esp_sleep_get_wakeup_cause();
   Serial.print("Wakeup reason: ");
@@ -63,13 +111,15 @@ void setup() {
     case ESP_SLEEP_WAKEUP_UNDEFINED : Serial.println("Undefined"); break;
     default : Serial.println("Other"); break;
   }
-
+  
   if (soilValue > dryThreshold) {
     digitalWrite(PUMP_PIN, HIGH);  // เปิดปั๊ม
     Serial.println("Pump ON (IMMEDIATELY SOIL DRY > 3500)");
     delay(5000);  // เปิดปั๊ม 1 ชั่วโมง
     digitalWrite(PUMP_PIN, LOW);   // ปิดปั๊ม
     Serial.println("Pump OFF");
+
+    delay(5000);
   } else {
     digitalWrite(PUMP_PIN, LOW); 
   }
@@ -90,10 +140,15 @@ void setup() {
       Serial.println("Pump OFF");
     }
   }
+
+  int soil_percent = map(soilValue, 0, 4095, 100, 0);
+  int pH_percent = map(pHValue, 0, 4095, 0, 100);
+  sendData(temp, pH_percent, soil_percent);
+
   // ตั้งเวลาให้ ESP32 ตื่นทุกๆ 1 ชั่วโมง (3600 วินาที)
   Serial.println("Wake up...");
   //esp_sleep_enable_timer_wakeup(3600 * 1000000);
-  esp_sleep_enable_timer_wakeup(360 * 1000000);
+  esp_sleep_enable_timer_wakeup(36 * 1000000);
   
   Serial.flush(); 
   Serial.println("Entering Deep Sleep...");
@@ -104,5 +159,5 @@ void setup() {
 }
 
 void loop() {
-  
+
 }

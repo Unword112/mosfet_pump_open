29036a72ffdbebf36ca86f504b7380c32dec670a
Bsl Nathakith <54128035+Unword112@users.noreply.github.com>
2025-04-21 02:16:54 +0700

    example Slave

--- sketch_apr15a.ino
+++ sketch_apr15a.ino
@@ -1,62 +1,50 @@
+#include <SPI.h>
 #include <LoRa.h>
 
-#define SS 5
-#define RST 22
-#define DIO0 4
-#define RELAY_PIN 32
-#define WAKE_PIN GPIO_NUM_4
+//define the pins used by the transceiver module
+#define ss 5
+#define rst 14
+#define dio0 2
 
 void setup() {
+  //initialize Serial Monitor
   Serial.begin(115200);
-  pinMode(RELAY_PIN, OUTPUT);
-  digitalWrite(RELAY_PIN, LOW);
+  while (!Serial);
+  Serial.println("LoRa Receiver");
 
-  esp_sleep_wakeup_cause_t wakeup_reason = esp_sleep_get_wakeup_cause();
-  Serial.print("Wakeup reason: ");
-  switch (wakeup_reason) {
-    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("EXT0 Wakeup (LoRa DIO0)"); break;
-    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("TIMER"); break;
-    default : Serial.println("Other"); break;
+  //setup LoRa transceiver module
+  LoRa.setPins(ss, rst, dio0);
+  
+  //replace the LoRa.begin(---E-) argument with your location's frequency 
+  //433E6 for Asia
+  //868E6 for Europe
+  //915E6 for North America
+  while (!LoRa.begin(868E6)) {
+    Serial.println(".");
+    delay(500);
   }
-
-  LoRa.setPins(SS, RST, DIO0);
-  if (!LoRa.begin(433E6)) {
-    Serial.println("LoRa init failed.");
-    while (true);
-  }
-  LoRa.receive();
-
-  if (wakeup_reason == ESP_SLEEP_WAKEUP_EXT0) {
-    Serial.println("Checking for LoRa packet...");
-    int packetSize = LoRa.parsePacket();
-    Serial.print("Packet size: ");
-    Serial.println(packetSize);
-
-    if (packetSize) {
-      String msg = "";
-      while (LoRa.available()) {
-        msg += (char)LoRa.read();
-      }
-      Serial.println("Received: " + msg);
-
-      if (msg == "ON") {
-        digitalWrite(RELAY_PIN, HIGH);
-        delay(5000);
-        digitalWrite(RELAY_PIN, LOW);
-        Serial.println("Relay activated.");
-      }
-    } else {
-      Serial.println("No LoRa packet received.");
-    }
-  }
-
-  esp_sleep_enable_ext0_wakeup(WAKE_PIN, 0);
-
-  Serial.println("Going to sleep...");
-  delay(100);
-  esp_deep_sleep_start();
+   // Change sync word (0xF3) to match the receiver
+  // The sync word assures you don't get LoRa messages from other LoRa transceivers
+  // ranges from 0-0xFF
+  LoRa.setSyncWord(0xF3);
+  Serial.println("LoRa Initializing OK!");
 }
 
 void loop() {
-  // ไม่ใช้งาน
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available()) {
+      String LoRaData = LoRa.readString();
+      Serial.print(LoRaData); 
+    }
+
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+  }
 }
